7dc041e7c3262516fdea9caddd295e670bdd8c5d
Emese Billing <talebilling@gmail.com>
2016-10-08 10:48:55 +0200

    Fibonacci variation one user input

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,9 @@
+import sys 
+
+list_size = eval(input())
 my_list = [0, 1]
 
-for i in range(2, 30):
+for i in range(2, list_size):
     value = (my_list[i-1]) + (my_list[i-2])
     my_list.append(value)
     
