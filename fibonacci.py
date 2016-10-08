84ee6f050c438332abb61683cebc4d5beddcde00
Emese Billing <talebilling@gmail.com>
2016-10-08 12:58:00 +0200

    fibonacci version 2 getting the idea of solution

--- fibonacci.py
+++ fibonacci.py
@@ -6,8 +6,12 @@ my_list = [0, 1]
 for i in range(2, list_size):
     value = (my_list[i-1]) + (my_list[i-2])
     my_list.append(value)
-    
+
+last_number = str(my_list[list_size-1])
+lengh = len(last_number)
+
 print('Fibonacci sequance:')
 
 for i in range(len(my_list)):
-    print("{}{} {}" .format (i+1, '.', my_list[i]))
+    a = str(my_list[i])
+    print("{}{} {}" .format (i+1, '.', a.rjust(lengh)))
